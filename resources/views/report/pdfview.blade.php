6fc9beeaa3ebcad7e408bad47732ff039ceca18c
Sonal <sonal@syslogyx.com>
2018-12-10 10:59:58 +0530

    commit after making adding footer in live server

--- resources/views/report/pdfview.blade.php
+++ resources/views/report/pdfview.blade.php
@@ -1,44 +1,37 @@
 <!doctype html>
 <html>
 	<style type="text/css">
-	
         @page {
-        header: page-header;
-        footer: page-footer;
+            header: page-header;
+            footer: page-footer;
+            /*margin-top: 100px;*/
+            margin-bottom: 150px;
+        }
+        .page-break {
+            page-break-after: always;
+            box-decoration-break: slice;
         }
        table th , td{
-        
-        padding-left: 0.35em;
-        padding-right: 0.35em;
-        padding-top: 0.35em;
-        padding-bottom: 0.35em;
-        vertical-align: top;
-        border:1px solid black;
-        
-        }
-        /*table td {
             padding-left: 0.35em;
             padding-right: 0.35em;
             padding-top: 0.35em;
             padding-bottom: 0.35em;
             vertical-align: top;
             border:1px solid black;
-
-        }*/
+        }
     </style>
     <body>
-       <!-- fund  inward log table  -->
         <htmlpageheader name="page-header">
             Machine Working Report
         </htmlpageheader>
         <hr>
         <h3 style="text-align:center;font-family:Calibri (Body);"> Machine Status Details</h3>
-        <div>
+        <div class="page-break">
             <table style="font-family:Calibri (Body);border-collapse: collapse;" width="100%">
                 <thead width="100%">
                     <tr>
                         <th width="7%" style="text-align:center">Sr No.</th>
-                        <th width="15%">Machine Name</th>
+                        <th width="15%" style="text-align:center">Machine Name</th>
                         <th width="13%" style="text-align:center">Off Time</th>
                         <th width="13%" style="text-align:center;">On Time</th>
                         <th width="14%" style="text-align:center;">Estimation Hour</th>
@@ -49,9 +42,7 @@
                 </thead>
                 <tbody>
                 	@foreach ($data as $key => $fn)
-
                     <tr>
-                      
                         <td style="text-align:center">{{++$key}}</td>
                         <td style="text-align:center">{{$fn['machine']['name']}}</td>
                         <td style="text-align: center;">{{$fn->created_at}}</td>
@@ -87,7 +78,10 @@
            <htmlpagefooter name="page-footer">
                 <hr>
                 <span style="text-align: center">{PAGENO}</span>
-               <!--  {DATE j-m-Y} -->
             </htmlpagefooter>
+            <!-- <htmlpagefooter name="page-footer" style="bottom: 0px">
+                <div style="text-align: center;color:#cac5c5">{PAGENO}</div>
+                <img src="{{ public_path('/img/Footer_.png') }}">
+            </htmlpagefooter> -->
     </body>
 </html>
